c0c61b701faa7bf439c05c0737cf7aec09ccb623
Cami Rodriguez <camiepisode@outlook.com>
2016-08-23 17:42:41 -0500

    Authenticating on frontend

--- Sigma.Website/Angular/Home/Controller.js
+++ Sigma.Website/Angular/Home/Controller.js
@@ -1,6 +1,26 @@
-﻿Sigma.controller("HomeController", function ($scope, equiposervice) {
+﻿Sigma.controller("HomeController", function ($scope, HomeService) {
 
     $scope.showAddUpdate = false;
-    $scope.test = "Hola";
+    $scope.LoginButtonText = "Login";
 
+    $scope.submitToLogin = function () {
+        $scope.MVCAction = "Login";
+        $scope.MVCController = "Dashboard";
+
+        var loginProcess = HomeService.Login($scope.Company, $scope.Username, $scope.Password);
+
+        loginProcess.then(function (response) {
+            console.log(response.data.Authentication);
+            $scope.LoginButtonText = "Login";
+            if (response.data.Authentication.Success == true) {
+                if (typeof (Storage) !== "undefined") {
+                    sessionStorage.setItem('Token', response.data.Authentication.Token);
+
+
+                } else {
+                    // Sorry! No Web Storage support..
+                }
+            }
+        })
+    }
 });
--- Sigma.Website/Angular/Home/Service.js
+++ Sigma.Website/Angular/Home/Service.js
@@ -1,3 +1,16 @@
 ﻿Sigma.service("HomeService", function ($http) {
+    this.Login = function (company, username, password) {
+        LoginButtonText = "Validating...";
 
+        var response = $http({
+            method: "post",
+            url: "/Dashboard/Login",
+            params: {
+                Company: company,
+                Email: username,
+                Password: password
+            }
+        });
+        return response;
+    }
 });
