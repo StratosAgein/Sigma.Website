08d6060d0475762da32d96c297af88de5b5f08b7
Cami Rodriguez <camiepisode@outlook.com>
2016-08-27 08:08:48 -0500

    added new entity

--- Sigma.Website/Models/Entities/PaymentMethod.cs
+++ Sigma.Website/Models/Entities/PaymentMethod.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sigma.Website.Models.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,10 @@ namespace Sigma.Website.Models.Entities
 {
     public class PaymentMethod
     {
+        public string _id { get; set; }
+        public PaymentMethodStatus PaymentMethodStatus { get; set; }
+        public string Name { get; set; }
+        public string Alias { get; set; }
+        public PaymentMethodType PaymentMethodType { get; set; }
     }
 }
---
+++ Sigma.Website/Models/Enums/PaymentMethodType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sigma.Website.Models.Enums
+{
+    public enum PaymentMethodType
+    {
+        CreditCard = 0,
+        PSE = 1,
+        DaviPlata = 2,
+        Nequi = 3
+    }
+}
